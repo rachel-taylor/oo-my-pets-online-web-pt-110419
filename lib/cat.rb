811adf8ab6b67c7956b98b9fe00dcae9b29e6a9c
Rachel T <rachmaybe@gmail.com>
2020-01-20 16:16:50 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,20 @@
 class Cat
+  attr_accessor :owner, :mood
   attr_reader :name
-  attr_accessor :mood
-
-  def initialize(name)
+  
+  @@all = []
+  
+  def initialize(name,owner)
     @name = name
+    @owner = owner
     @mood = "nervous"
+    @@all << self
   end
+  
+  def self.all
+    @@all
+  end
+  
 end
+
+
