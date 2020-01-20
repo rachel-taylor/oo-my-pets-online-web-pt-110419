0762d9f4a3e831b0cf5ad341f77f82ee7e0d017a
Rachel T <rachmaybe@gmail.com>
2020-01-20 16:17:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,17 @@
 class Dog
+  attr_accessor :owner, :mood
   attr_reader :name
-  attr_accessor :mood
-
-  def initialize(name)
+  
+  @@all = []
+  
+  def initialize(name,owner)
     @name = name
+    @owner = owner
     @mood = "nervous"
+    @@all << self
+  end
+  
+  def self.all
+    @@all
   end
 end
